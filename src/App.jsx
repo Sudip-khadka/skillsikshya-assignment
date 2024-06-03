e960165c003734456d9dbe6c3a98e5037adc825c
Sudip-khadka <ksudip0123@gmail.com>
2024-06-03 17:24:28 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Button from './component/button'
+
+
+
+function App() {
+  return (
+    <>
+      <div className='text'>
+        
+    <Button type="delete" className="delete"/> 
+    <Button type="play" className="play"/> 
+    <Button type="setting" className="setting"/> 
+    <Button type="home" className="home"/> 
+    <Button type="upload" className="upload"/> 
+    <Button type="cancel" className="cancel"/> 
+    <Button type="submit" className="submit"/> 
+    <Button type="icon" className="icon"/> 
+    <Button type="download" className="download"/> 
+    <Button type="search" className="search"/> 
+    
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/button.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Icon from './Icon';
+import '../btn.css'
+
+const Button= function Button({type, className}) {
+    switch(type){
+        case 'delete':
+            return <button className={className}><Icon type="delete"/>Delete</button>;
+        case 'home':
+            return <button className={className}><Icon type="home"/>Home</button>;
+        case 'setting':
+            return <button className={className}><Icon type="setting"/>Settings</button>;
+
+        case 'submit':
+            return <button className={className}>Submit</button>;
+        case 'upload':
+            return <button className={className}><Icon type="upload"/>Upload</button>;
+        case 'play':
+            return <button className={className}><Icon type="play"/>Play</button>;
+        case 'icon':
+            return <button className={className}>Icon</button>;
+        case 'search':
+            return <button className={className}>Search<Icon type="search"/></button>;
+        case 'download':
+            return <button className={className}><Icon type="download"/>Download</button>;
+    }
+  return (
+    
+    <button className={className}>{type}</button>
+    
+  )
+}
+export default Button;
